7e4d0fc0af75d97ba15749e8a804451ed882b36f
vitaliyoliynik <oliynikvs@gmail.com>
2026-05-08 12:04:43 +0200

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,26 +1,37 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings ):
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car):
-        price = car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
+    def calculate_washing_price(self, car: Car) -> float:
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
         return round(price, 1)
 
-    def wash_single_car(self, car):
-     if self.clean_power  > car.clean_mark:
-         car.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars):
+    def serve_cars(self, cars: list) -> float:
         total = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -28,9 +39,9 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total, 1)
 
-    def rate_service(self, rate):
-            self.average_rating = round(
-                (self.average_rating * self.count_of_ratings + rate)
-                / (self.count_of_ratings + 1), 1
-            )
-            self.count_of_ratings += 1
+    def rate_service(self, rate: float) -> None:
+        self.average_rating = round(
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1), 1
+        )
+        self.count_of_ratings += 1
